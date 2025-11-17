5df851151646d902f17543fd8d43e4ced1916c24
zajcevi0111-ai <zajcevi0111@gmail.com>
2025-11-17 17:57:30 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+import os
+
+def init_database():
+    print("✅ SQL Injection Lab - База данных")
+    # База будет реализована здесь
+    pass
+
+if __name__ == "__main__":
+    init_database()
